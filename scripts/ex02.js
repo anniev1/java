cdf0e16c8ebc8469a808380f823a24b0d69579c0
annie <annie_marie@estudante.sesisenai.org.br>
2026-03-25 11:40:33 -0300

    add - exs02

--- scripts/ex02.js
+++ scripts/ex02.js
@@ -1,15 +1,9 @@
 function lancamento() {
     let numero = 10;
-    let continuar = true;
-    let numero = parseFloat(prompt("Digite um número (ou 'sair' para encerrar):"));
-    if (numero < 0) {
-       { else {
-            aalert("Número negativo não é permitido para lançamento!");
-       }}
-        continuar = confirm("Deseja continuar lançando números?");
-        }
+    
+    for (numero = 10; numero >= 0; numero--) {
+        console.log(numero);
     }
-{
+
     alert("Lançamento realizado!");
 }
-   
